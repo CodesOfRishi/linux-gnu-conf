cab83fe741dad81a83c903ed710268517e25662e
Rishi Kumar <contact.rishikmr@gmail.com>
2021-06-18 10:16:00 +0530

    improve bin/showPath.sh (efficient than before)

--- bin/showPaths.sh
+++ bin/showPaths.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-VAL=1
-while [ `echo $PATH"$" | cut -c$VAL` != "$" ]; do
-	if [ `echo $PATH | cut -c$VAL` = ":" ]; then 
-		echo 
-	else 
-		echo -n `echo $PATH | cut -c$VAL`
-	fi
-	VAL=$(($VAL+1))
-done
-echo 
+echo "${PATH//:/$'\n'}"
