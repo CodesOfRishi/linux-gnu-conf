9a7dc47cab25e27a19e3af6fe887b2cd647a795a
Rishi Kumar <contact.rishikmr@gmail.com>
2021-06-29 09:48:44 +0530

    overload operator<< for std::ostream& for dbg(…) to work for std::priority_queue data structure.

--- others/code-compete/.basic-template.cpp
+++ others/code-compete/.basic-template.cpp
@@ -7,22 +7,37 @@
 #include "/home/rishi/.local/include/dbg.h"
 
 #ifdef _GLIBCXX_STACK
-	template<typename T>
-	std::ostream& operator<<(std::ostream& out, std::stack<T> _st) {
-		out << "{";
-		while (!_st.empty()) {
-			out << _st.top();
-			_st.pop();
+template<typename T>
+std::ostream& operator<<(std::ostream& out, std::stack<T> _st) {
+	out << "{";
+	while (!_st.empty()) {
+		out << _st.top();
+		_st.pop();
 
-			if (!_st.empty()) out << " ";
-		}
-		out << "}";
-		return out;
+		if (!_st.empty()) out << " ";
 	}
+	out << "}";
+	return out;
+}
+#endif
+
+#ifdef _GLIBCXX_QUEUE
+template<typename T>
+std::ostream& operator<<(std::ostream& out, std::priority_queue<T> _pq) {
+	out << "{";
+	while (!_pq.empty()) {
+		out << _pq.top();
+		_pq.pop();
+
+		if (!_pq.empty()) out << " ";
+	}
+	out << "}";
+	return out;
+}
 #endif
 
 #else 
-	template<typename... Types> void dbg(const Types&... x) { /* nothing!! */ }
+template<typename... Types> void dbg(const Types&... x) { /* nothing!! */ }
 #endif
 
 using namespace std;
