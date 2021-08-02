04e673268852f9fb8e77f5c429511e4e316a3a72
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-02 22:03:30 +0530

    nvim-treesitter: Ensure installed parser for lua.

--- .config/nvim/lua/plugins-conf/nvim-treesitter.lua
+++ .config/nvim/lua/plugins-conf/nvim-treesitter.lua
@@ -1,5 +1,5 @@
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "java", "toml", "bash", "python" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = { "c", "cpp", "java", "toml", "bash", "python", "lua" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   ignore_install = {}, -- List of parsers to ignore installing
   highlight = {
     enable = true,              -- false will disable the whole extension
