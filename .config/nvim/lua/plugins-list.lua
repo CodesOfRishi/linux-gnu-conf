56b8bd445276b9141f506e12615b07769d4e7758
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-02 21:54:19 +0530

    Include configurations of plugins that are short enough to be included in the plugins' specifications.

--- .config/nvim/lua/plugins-list.lua
+++ .config/nvim/lua/plugins-list.lua
@@ -92,25 +92,38 @@ return require('packer').startup(function()
 
 	-- Terminal
 	use { 'voldikss/vim-floaterm', --{{{
-		config = function() require'plugins-conf.vim-floaterm' end,
+		config = function() 
+			vim.g.floaterm_width = 0.8
+			vim.g.floaterm_height = 0.7
+		end,
 		cmd = { "FloatermNew", "FloatermToggle", "FloatermNext", "FloatermPrev", "FloatermFirst", "FloatermLast", "FloatermUpdate", "FloatermShow", "FloatermSend" }
 	}--}}}
 
 	-- UI
 	use { 'folke/tokyonight.nvim',--{{{
-		config = function() require'plugins-conf.themes' end,
+		config = function() 
+			vim.g.tokyonight_style = "night" -- night OR storm 
+			vim.cmd[[colorscheme tokyonight]]
+		end,
 		disable = true,
 		lock = true
 	}--}}}
 
 	use { 'marko-cerovac/material.nvim', --{{{
-		config = function() require'plugins-conf.themes' end,
+		config = function() 
+			vim.g.material_style = "deep ocean" -- darker OR deep ocean OR lighter OR oceanic OR palenight
+			require('material').set()
+		end,
 		disable = true,
 		lock = true
 	}--}}}
 
 	use { 'navarasu/onedark.nvim',--{{{
-		config = function() require'plugins-conf.themes' end
+		config = function() 
+			vim.g.onedark_transparent_background = 1
+			vim.g.onedark_style = "deep" -- dark OR darker OR cool OR deep OR warm OR warmer
+			require('onedark').setup()
+		end
 	}--}}}
 
 	use { 'hoob3rt/lualine.nvim', --{{{
