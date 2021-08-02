d93da13cfa1285d71f9011902e985aa40b31d547
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-02 14:36:24 +0530

    Add plugins configurations in nvim/lua/plugins-conf/

---
+++ linux-conf/.config/nvim/lua/plugins-conf/lualine.lua
@@ -0,0 +1,31 @@
+require'lualine'.setup {
+	options = {
+    icons_enabled = true,
+    theme = 'onedark',
+    section_separators = {'', ''},
+    component_separators = {'', ''},
+    disabled_filetypes = {}
+	},
+	sections = {
+	  lualine_a = {'mode'},
+	  lualine_b = {'branch', 'b:gitsigns_status'},
+	  lualine_c = {{'filename', path = 1}},
+	  lualine_x = {
+	    {'diagnostics', sources = {"nvim_lsp"}, symbols = {error = ' ', warn = ' ', info = ' ', hint = ' '}},
+	    'filetype',
+	    'encoding'
+	  },
+	  lualine_y = {'progress'},
+	  lualine_z = {'location'}
+	},
+	inactive_sections = {
+	  lualine_a = {},
+	  lualine_b = {},
+	  lualine_c = {'filename'},
+	  lualine_x = {'location'},
+	  lualine_y = {},
+	  lualine_z = {}
+	},
+	tabline = {},
+	extensions = {'fugitive'}
+}
