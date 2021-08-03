e2f19acec57e1bc440bb9ee806eab52cf425e18b
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-03 23:18:09 +0530

    Neovim: Install telescope.nvim plugin & re-configure mappings for lspsaga

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -71,11 +71,13 @@ map("i", "<C-b>", "compe#scroll({ 'delta': -4 }", { expr = true })
 
 -- lspsaga plugin{{{
 -- lsp provider to find the cursor word definition and reference
-map("n", "gh", "<Cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", {})
+-- NOTE: mapped to Telescope's lsp picker
+-- map("n", "gh", "<Cmd>lua require'lspsaga.provider'.lsp_finder()<CR>", {})
 
 -- code action
-map("n", "<leader>a", "<Cmd>lua require('lspsaga.codeaction').code_action()<CR>", {})
-map("v", "<leader>a", "<Cmd><C-u>lua require('lspsaga.codeaction').range_code_action()<CR>", {})
+-- NOTE: mapped to Telescope's lsp picker
+--[[ map("n", "<leader>a", "<Cmd>lua require('lspsaga.codeaction').code_action()<CR>", {})
+map("v", "<leader>a", "<Cmd><C-u>lua require('lspsaga.codeaction').range_code_action()<CR>", {}) ]]
 
 -- hover
 map("n", "K", "<Cmd>lua require('lspsaga.hover').render_hover_doc()<CR>", {})
@@ -86,16 +88,17 @@ map("n", "<C-b>", "<Cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)
 map("n", "gs", "<Cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>", {})
 
 -- renaming
-map("n", "gr", "<Cmd>lua require('lspsaga.rename').rename()<CR>", {})
+map("n", "<leader>rn", "<Cmd>lua require('lspsaga.rename').rename()<CR>", {})
 
 -- preview definition
-map("n", "gd", "<Cmd>lua require'lspsaga.provider'.preview_definition()<CR>", {})
+-- NOTE: mapped to Telescope's lsp picker
+-- map("n", "gd", "<Cmd>lua require'lspsaga.provider'.preview_definition()<CR>", {})
 
 -- show diagnostics
-map("n", "<leader>d", "<Cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", {})
+map("n", "<leader>dd", "<Cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>", {})
 
 -- only show diagnostics if cursor is over the area
-map("n", "<leader>c", "<Cmd>lua require'lspsaga.diagnostic'.show_cursor_diagnostics()<CR>", {})
+map("n", "<leader>dc", "<Cmd>lua require'lspsaga.diagnostic'.show_cursor_diagnostics()<CR>", {})
 
 -- jump diagnostics
 map("n", "[e", "<Cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>", {})
@@ -116,3 +119,22 @@ map("n", "<leader>ut", "<Cmd>UndotreeToggle<CR>", {})--}}}
 
 -- vim-floaterm{{{
 map("n", "tt", "<Cmd>FloatermToggle<CR>", {})--}}}
+
+-- telescope{{{
+map("n", "<leader>ff", "<Cmd>Telescope find_files hidden=true<CR>", {})
+map("n", "<leader>fg", "<Cmd>Telescope live_grep<CR>", {})
+map("n", "<leader>fG", "<Cmd>Telescope live_grep grep_open_files=true<CR>", {})
+map("n", "<leader>ls", "<Cmd>Telescope file_browser hidden=true<CR>", {})
+
+-- telescope lsp
+map("n", "gh", "<Cmd>Telescope lsp_references theme=get_cursor<CR>", {})
+map("n", "gd", "<Cmd>Telescope lsp_definitions theme=get_cursor<CR>", {})
+map("n", "<leader>a", "<Cmd>Telescope lsp_code_actions theme=get_cursor<CR>", {})
+map("n", "<leader>dw", "<Cmd>Telescope lsp_workspace_diagnostics<CR>", {})
+map("n", "<leader>da", "<Cmd>Telescope lsp_document_diagnostics<CR>", {})
+
+-- telescope git
+map("n", "<C-g><C-h>", "<Cmd>Telescope git_commits<CR>", {})
+map("n", "<C-g><C-f>", "<Cmd>Telescope git_status<CR>", {})
+map("n", "<C-g><C-b>", "<Cmd>Telescope git_branches<CR>", {})
+--}}}
--- .config/nvim/lua/plugins-list.lua
+++ .config/nvim/lua/plugins-list.lua
@@ -73,6 +73,12 @@ return require('packer').startup(function()
 		event = "BufRead"
 	}--}}}
 
+	use { 'nvim-telescope/telescope.nvim', --{{{
+		cmd = { "Telescope" },
+		requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}},
+		config = function() require'plugins-conf.telescope' end
+	}--}}}
+
 	-- Git
 	use 'tpope/vim-fugitive'
 
