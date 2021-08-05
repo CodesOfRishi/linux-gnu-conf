ee0a535119a0f726020276ef108b634d40d1bd1f
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-05 23:13:32 +0530

    Nvim: Telescope: Map 'builtin.commands' picker.

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -125,6 +125,7 @@ map("n", "<leader>ff", "<Cmd>Telescope find_files hidden=true<CR>", {})
 map("n", "<leader>fg", "<Cmd>Telescope live_grep<CR>", {})
 map("n", "<leader>fG", "<Cmd>Telescope live_grep grep_open_files=true<CR>", {})
 map("n", "<leader>ls", "<Cmd>Telescope file_browser hidden=true<CR>", {})
+map("n", "<leader>fc", "<Cmd>Telescope commands<CR>", {})
 
 -- telescope lsp
 map("n", "gd", "<Cmd>Telescope lsp_definitions theme=get_cursor<CR>", {})
