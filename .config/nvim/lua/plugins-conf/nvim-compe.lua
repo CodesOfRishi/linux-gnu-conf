3e4a55715d7c543527f1ef9a4bfee18eac1e460c
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-21 15:05:36 +0530

    Nvim: Refactoring

--- .config/nvim/lua/plugins-conf/nvim-compe.lua
+++ .config/nvim/lua/plugins-conf/nvim-compe.lua
@@ -25,13 +25,13 @@ require'compe'.setup {
 	};
 
 	source = {
-	path = true;
-	buffer = true;
-	calc = true;
-	nvim_lsp = true;
-	nvim_lua = true;
-	vsnip = true;
-	ultisnips = true;
-	luasnip = true;
+		path = true;
+		buffer = true;
+		calc = true;
+		nvim_lsp = true;
+		nvim_lua = true;
+		vsnip = true;
+		ultisnips = true;
+		luasnip = true;
 	};
 }
