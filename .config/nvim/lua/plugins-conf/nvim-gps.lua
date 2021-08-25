0d5e98c677cdb086f43e6b1ad2c90196d3bcc173
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-25 10:45:06 +0530

    Nvim: nvim-gps.lua: change icons for "function-name" & "method-name"

--- .config/nvim/lua/plugins-conf/nvim-gps.lua
+++ .config/nvim/lua/plugins-conf/nvim-gps.lua
@@ -1,8 +1,8 @@
 require("nvim-gps").setup({
 	icons = {
 		["class-name"] = ' ',      -- Classes and class-like objects
-		["function-name"] = ' ',   -- Functions
-		["method-name"] = ' '      -- Methods (functions inside class-like objects)
+		["function-name"] = ' ',   -- Functions
+		["method-name"] = ' '      -- Methods (functions inside class-like objects)
 	},
 	languages = {                    -- You can disable any language individually here
 		["c"] = true,
