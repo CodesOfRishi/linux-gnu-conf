47f20fe894a505340a8d24f571f60d611d7f41a4
Rishi Kumar <contact.rishikmr@gmail.com>
2021-08-26 09:35:24 +0530

    Nvim: `require` mapping template in files in ftplugin

--- .config/nvim/ftplugin/cpp.lua
+++ .config/nvim/ftplugin/cpp.lua
@@ -1,14 +1,7 @@
 -- change comment style for cpp file
 vim.api.nvim_buf_set_option("0", "commentstring", "// %s")
 
--- template for mapping
-local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true, silent = true}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local map = require("utils").map
 
 -- create mappings for compiling & running C++ files
 map("n", "<C-p>", ":FloatermNew makecp %<CR>", {})
--- .config/nvim/ftplugin/python.lua
+++ .config/nvim/ftplugin/python.lua
@@ -1,11 +1,4 @@
--- template for mapping
-local function map(mode, lhs, rhs, opts)
-    local options = {noremap = true, silent = true}
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
-end
+local map = require("utils").map
 
 -- create mappings for running python scripts
 -- make sure script contains the shebang specifying the interpreter &,
