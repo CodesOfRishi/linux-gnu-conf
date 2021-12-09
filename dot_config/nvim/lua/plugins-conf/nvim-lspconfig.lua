fbf2a7c1ff1806c3022f927a107cd788722e5221
Rishi Kumar <contact.rishikmr@gmail.com>
2021-12-10 00:51:59 +0530

    nvim-lspconfig.lua: `vim.lsp.diagnostic` is deprecated, instead use `vim.diagnostic`

--- dot_config/nvim/lua/plugins-conf/nvim-lspconfig.lua
+++ dot_config/nvim/lua/plugins-conf/nvim-lspconfig.lua
@@ -32,11 +32,4 @@ local on_attach = function(client, bufnr)
 	buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 end
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-    vim.lsp.diagnostic.on_publish_diagnostics, { 
-		virtual_text = false,
-		signs = true, 
-		update_in_insert = false, 
-		underline = true,
-    }
-)
+vim.diagnostic.config({virtual_text = false})
