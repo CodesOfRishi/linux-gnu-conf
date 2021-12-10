6f143daddc83de8b58723a0261c5ca70e4052d66
Rishi Kumar <contact.rishikmr@gmail.com>
2021-12-10 22:04:18 +0530

    Nvim: autocmds.lua: configure diagnostic-signs

--- dot_config/nvim/lua/autocmds.lua
+++ dot_config/nvim/lua/autocmds.lua
@@ -15,3 +15,10 @@ vim.cmd([[
 vim.cmd([[
 	au TextYankPost * silent! lua vim.highlight.on_yank { timeout = 450 }
 ]])
+
+vim.cmd([[
+	sign define DiagnosticSignError text= texthl=DiagnosticSignError
+	sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn
+	sign define DiagnosticSignInfo text= texthl=DiagnosticSignInfo
+	sign define DiagnosticSignHint text= texthl=DiagnosticSignHint
+]])
