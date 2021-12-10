e5a0ceeacac861be75ed4eafd2805c00b963899b
Rishi Kumar <contact.rishikmr@gmail.com>
2021-12-10 20:48:43 +0530

    Nvim: install & configure nvim-notify plugin.

--- dot_config/nvim/lua/plugins-list.lua
+++ dot_config/nvim/lua/plugins-list.lua
@@ -14,6 +14,11 @@ return require('packer').startup(function()
 	-- Packer can manage itself 
 	use 'wbthomason/packer.nvim'
 
+	-- notification{{{
+	use { 'rcarriga/nvim-notify', 
+		config = function() require ("plugins-conf.notify") end
+	}--}}}
+	
 	-- completion
 	use { 'hrsh7th/nvim-cmp', --{{{
 		requires = { "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lua", "hrsh7th/cmp-path", "hrsh7th/cmp-calc" },
